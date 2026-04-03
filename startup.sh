9bef8106d96c25038c671dc62d2d08ab4212c39d
WhoamiI00 <ankitisop12@gmail.com>
2026-04-03 19:48:36 +0530

    deployment updates

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ trap 'kill $(jobs -p)' EXIT
 
 python manage.py migrate --noinput
 python manage.py qcluster &
-gunicorn animal.wsgi --bind=0.0.0.0:8000 -w 1
+gunicorn animal.wsgi --bind=0.0.0.0 --timeout 600 -w 1

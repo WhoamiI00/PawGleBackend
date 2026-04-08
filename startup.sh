63c0f723d39107375f4beaf851eaf70bd694b559
WhoamiI00 <ankitisop12@gmail.com>
2026-04-08 17:39:48 +0530

    bind gunicorn to PORT env var with access logs

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ trap 'kill $(jobs -p)' EXIT
 
 python manage.py migrate --noinput
 python manage.py qcluster &
-gunicorn animal.wsgi --bind=0.0.0.0 --timeout 600 -w 1
+gunicorn animal.wsgi --bind=0.0.0.0:${PORT:-8000} --timeout 600 -w 1 --access-logfile '-' --error-logfile '-'
